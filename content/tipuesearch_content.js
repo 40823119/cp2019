63b2e07129562928b096a63a7578dd4bc5dbaa2d
40823119 <40823119@gm.nfu.edu.tw>
2019-09-26 02:42:09 +0800

    initnial add

---
+++ content/tipuesearch_content.js
@@ -0,0 +1,1 @@
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'HAHA.pdf \xa0 \n About \n http://mde.tw/cad2019 \xa0', 'tags': '', 'url': 'About.html'}, {'title': 'USB', 'text': '', 'tags': '', 'url': 'USB.html'}, {'title': 'USB2', 'text': '', 'tags': '', 'url': 'USB2.html'}]};
